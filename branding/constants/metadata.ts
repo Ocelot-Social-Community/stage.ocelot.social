1d6224ef8510c1fc8a55a4b8a0b17d9fdc2b381c
Wolfgang Huß <wolle.huss@pjannto.com>
2024-10-20 12:26:53 +0200

    Set metadata data in TS

--- branding/constants/metadata.ts
+++ branding/constants/metadata.ts
@@ -1,9 +1,9 @@
 // this file is duplicated in `backend/src/config/metadata.js` and `webapp/constants/metadata.js` and replaced on rebranding
 export default {
-  APPLICATION_NAME: 'ocelot.social',
-  APPLICATION_SHORT_NAME: 'ocelot.social',
-  APPLICATION_DESCRIPTION: 'Ocelot Social Community',
-  COOKIE_NAME: 'ocelot-social-token',
-  ORGANIZATION_NAME: 'busFaktor e.V.',
-  ORGANIZATION_JURISDICTION: 'Deutschland',
+  APPLICATION_NAME: 'Reformer.network',
+  APPLICATION_SHORT_NAME: 'Reformer.network',
+  APPLICATION_DESCRIPTION: 'Reformer.network, social network',
+  COOKIE_NAME: 'reformer-network-token',
+  ORGANIZATION_NAME: 'Stichting Rudulin',
+  ORGANIZATION_JURISDICTION: 'Amsterdam',
 }
