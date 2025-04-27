3080d83540cc903786d1da0249a754c4b7bf068f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-04-27 13:33:18 +0200

    fix neode model name

--- branding/data/badges-branding.ts
+++ branding/data/badges-branding.ts
@@ -7,61 +7,61 @@ const neode = getNeode()
 
 export const trophies = async () => {
   return {
-    trophyBeitragsblocker: await neode.create('badge', {
+    trophyBeitragsblocker: await neode.create('Badge', {
       id: 'trophy_blue_beitragsblocker',
       type: 'trophy',
       description: 'Hat bei beitragsblocker.de mitgemacht',
       icon: '/img/badges/trophy_blue_beitragsblocker.svg',
     }),
-    trophyLiberationexpress: await neode.create('badge', {
+    trophyLiberationexpress: await neode.create('Badge', {
       id: 'trophy_blue_liberationexpress',
       type: 'trophy',
       description: 'Hat bei liberation-express.com mitgemacht',
       icon: '/img/badges/trophy_blue_liberationexpress.svg',
     }),
-    trophyMasernimpfblocker: await neode.create('badge', {
+    trophyMasernimpfblocker: await neode.create('Badge', {
       id: 'trophy_blue_masernimpfblocker',
       type: 'trophy',
       description: 'Hat bei masern-impfblocker.de mitgemacht',
       icon: '/img/badges/trophy_blue_masernimpfblocker.svg',
     }),
-    trophyMaskenfreiexpress: await neode.create('badge', {
+    trophyMaskenfreiexpress: await neode.create('Badge', {
       id: 'trophy_blue_maskenfreiexpress',
       type: 'trophy',
       description: 'Hat bei maskenfrei.express mitgemacht',
       icon: '/img/badges/trophy_blue_maskenfreiexpress.svg',
     }),
-    trophyNachweisexpress: await neode.create('badge', {
+    trophyNachweisexpress: await neode.create('Badge', {
       id: 'trophy_blue_nachweisexpress',
       type: 'trophy',
       description: 'Hat bei nachweis-express.de mitgemacht',
       icon: '/img/badges/trophy_blue_nachweisexpress.svg',
     }),
-    trophyRundfunkalarm: await neode.create('badge', {
+    trophyRundfunkalarm: await neode.create('Badge', {
       id: 'trophy_blue_rundfunkalarm.svg',
       type: 'trophy',
       description: 'Hat bei rundfunkalarm.de mitgemacht',
       icon: '/img/badges/trophy_blue_rundfunkalarm.svg',
     }),
-    trophyTestexpress: await neode.create('badge', {
+    trophyTestexpress: await neode.create('Badge', {
       id: 'trophy_blue_testexpress',
       type: 'trophy',
       description: 'Hat bei test-express.de mitgemacht',
       icon: '/img/badges/trophy_blue_testexpress.svg',
     }),
-    trophyBeitragsstopper: await neode.create('badge', {
+    trophyBeitragsstopper: await neode.create('Badge', {
       id: 'trophy_blue_beitragsstopper',
       type: 'trophy',
       description: 'Hat bei beitragsstopper.de mitgemacht',
       icon: '/img/badges/trophy_blue_beitragsstopper.svg',
     }),
-    trophyFreundeDerDemokratie: await neode.create('badge', {
+    trophyFreundeDerDemokratie: await neode.create('Badge', {
       id: 'trophy_orange_freunde_der_demokratie',
       type: 'trophy',
       description: 'Hat bei freundederdemokratie.org mitgemacht',
       icon: '/img/badges/trophy_orange_freunde_der_demokratie.svg',
     }),
-    trophyRestartDemocracy: await neode.create('badge', {
+    trophyRestartDemocracy: await neode.create('Badge', {
       id: 'trophy_orange_restart_democracy',
       type: 'trophy',
       description: 'Hat bei restart-democracy.org mitgemacht',
@@ -72,97 +72,97 @@ export const trophies = async () => {
 
 export const verification = async () => {
   return {
-    verificationBadenWuerttemberg: await neode.create('badge', {
+    verificationBadenWuerttemberg: await neode.create('Badge', {
       id: 'verification_baden_wuerttemberg',
       type: 'verification',
       description: 'Kommt aus Baden-Württemberg',
       icon: '/img/badges/verification_baden_wuerttemberg.svg',
     }),
-    verificationBayern: await neode.create('badge', {
+    verificationBayern: await neode.create('Badge', {
       id: 'verification_bayern',
       type: 'verification',
       description: 'Kommt aus Bayern',
       icon: '/img/badges/verification_bayern.svg',
     }),
-    verificationBerlin: await neode.create('badge', {
+    verificationBerlin: await neode.create('Badge', {
       id: 'verification_berlin',
       type: 'verification',
       description: 'Kommt aus Berlin',
       icon: '/img/badges/verification_berlin.svg',
     }),
-    verificationBrandenburg: await neode.create('badge', {
+    verificationBrandenburg: await neode.create('Badge', {
       id: 'verification_brandenburg',
       type: 'verification',
       description: 'Kommt aus Brandenburg',
       icon: '/img/badges/verification_brandenburg.svg',
     }),
-    verificationBremen: await neode.create('badge', {
+    verificationBremen: await neode.create('Badge', {
       id: 'verification_bremen',
       type: 'verification',
       description: 'Kommt aus Bremen',
       icon: '/img/badges/verification_bremen.svg',
     }),
-    verificationHamburg: await neode.create('badge', {
+    verificationHamburg: await neode.create('Badge', {
       id: 'verification_hamburg',
       type: 'verification',
       description: 'Kommt aus Hamburg',
       icon: '/img/badges/verification_hamburg.svg',
     }),
-    verificationHessen: await neode.create('badge', {
+    verificationHessen: await neode.create('Badge', {
       id: 'verification_hessen',
       type: 'verification',
       description: 'Kommt aus Hessen',
       icon: '/img/badges/verification_hessen.svg',
     }),
-    verificationMecklenburgVorpommern: await neode.create('badge', {
+    verificationMecklenburgVorpommern: await neode.create('Badge', {
       id: 'verification_mecklenburg_vorpommern',
       type: 'verification',
       description: 'Kommt aus Mecklenburg-Vorpommern',
       icon: '/img/badges/verification_mecklenburg_vorpommern.svg',
     }),
-    verificationNiedersachsen: await neode.create('badge', {
+    verificationNiedersachsen: await neode.create('Badge', {
       id: 'verification_niedersachsen',
       type: 'verification',
       description: 'Kommt aus Niedersachsen',
       icon: '/img/badges/verification_niedersachsen.svg',
     }),
-    verificationNordrheinWestfalen: await neode.create('badge', {
+    verificationNordrheinWestfalen: await neode.create('Badge', {
       id: 'verification_nordrhein_westfalen',
       type: 'verification',
       description: 'Kommt aus Nordrhein-Westfalen',
       icon: '/img/badges/verification_nordrhein_westfalen.svg',
     }),
-    verificationRheinlandPfalz: await neode.create('badge', {
+    verificationRheinlandPfalz: await neode.create('Badge', {
       id: 'verification_rheinland_pfalz',
       type: 'verification',
       description: 'Kommt aus Rheinland-Pfalz',
       icon: '/img/badges/verification_rheinland_pfalz.svg',
     }),
-    verificationSaarland: await neode.create('badge', {
+    verificationSaarland: await neode.create('Badge', {
       id: 'verification_saarland',
       type: 'verification',
       description: 'Kommt aus dem Saarland',
       icon: '/img/badges/verification_saarland.svg',
     }),
-    verificationSachsenAnhalt: await neode.create('badge', {
+    verificationSachsenAnhalt: await neode.create('Badge', {
       id: 'verification_sachsen_anhalt',
       type: 'verification',
       description: 'Kommt aus Sachsen-Anhalt',
       icon: '/img/badges/verification_sachsen_anhalt.svg',
     }),
-    verificationSachsen: await neode.create('badge', {
+    verificationSachsen: await neode.create('Badge', {
       id: 'verification_sachsen',
       type: 'verification',
       description: 'Kommt aus Sachsen',
       icon: '/img/badges/verification_sachsen.svg',
     }),
-    verificationSchleswigHolstein: await neode.create('badge', {
+    verificationSchleswigHolstein: await neode.create('Badge', {
       id: 'verification_schleswig_holstein',
       type: 'verification',
       description: 'Kommt aus Schleswig-Holstein',
       icon: '/img/badges/verification_schleswig_holstein.svg',
     }),
-    verificationThueringen: await neode.create('badge', {
+    verificationThueringen: await neode.create('Badge', {
       id: 'verification_thueringen',
       type: 'verification',
       description: 'Kommt aus Thüringen',
