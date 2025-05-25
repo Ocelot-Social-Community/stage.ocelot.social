7190657326970f77b6d327fa859981f5269c76b1
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-05-25 15:52:31 +0200

    auto verify user when setting an appropriate location

--- branding/middlewares/brandingMiddlewares.ts
+++ branding/middlewares/brandingMiddlewares.ts
@@ -2,7 +2,11 @@
 import { MiddlewareOrder } from '@middleware/index'
 
 import badgeLookup from './badgeLookup'
+import locationVerification from './locationVerification'
 
 export default (): MiddlewareOrder[] => {
-  return [{ order: 0, name: 'badgeLookup', middleware: badgeLookup }]
+  return [
+    { order: 0, name: 'badgeLookup', middleware: badgeLookup },
+    { order: 1, name: 'locationVerification', middleware: locationVerification },
+  ]
 }
